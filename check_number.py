f998197e65208394df11d341700d6c34403ba428
Seth Weidman <sethhweidman@gmail.com>
2017-07-15 11:52:27 -0500

    Render template with arguments works

---
+++ check_number.py
@@ -0,0 +1,6 @@
+def check_number(num):
+    int_num = int(num)
+    if int_num % 2 == 1:
+        return "The number is odd"
+    else:
+        return "The number is even"
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from check_number import check_number
 
 app = Flask(__name__)
 
@@ -7,8 +8,10 @@ def hello_world():
     return render_template('index.html')
 
 @app.route('/check_number/', methods=['POST'])
-def check_number():
-    return "The form was submitted"
+def check_number_page():
+    number = request.form['number']
+    number_message = check_number(number)
+    return render_template('number_answer.html', number_message=number_message)
 
 if __name__ == '__main__':
     app.run(debug=True)
