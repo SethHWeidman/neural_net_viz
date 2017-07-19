270fe5d948bf8807007e7782382786deba4a37c3
Seth Weidman <sethhweidman@gmail.com>
2017-07-19 11:11:26 -0500

    Form changes appearance of neurons

--- main.py
+++ main.py
@@ -3,9 +3,19 @@ from check_number import check_number
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/visualize/')
 def hello_world():
-    return render_template('index.html')
+    return render_template('index_data_imputed.html')
+
+@app.route('/visualize_neurons/', methods=['GET', 'POST'])
+def visualize_neurons():
+    number = request.form['number']
+    number_final = int(number) * 5
+    ys = [50, 150, 250, 350]
+    xs = [number_final for y in ys]
+    zip_x_y = [dict({"x": i, "y": j}) for i, j in zip(xs, ys)]
+    return render_template('index_data_imputed.html',
+                           data=zip_x_y)
 
 @app.route('/check_number/', methods=['POST'])
 def check_number_page():
