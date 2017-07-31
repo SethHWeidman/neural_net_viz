af745a3ee675d6bd077a68f72ed7fd89f952f977
Seth Weidman <sethhweidman@gmail.com>
2017-07-31 06:46:21 -0500

    Hacky way of maintaining state

--- main.py
+++ main.py
@@ -7,11 +7,17 @@ app = Flask(__name__)
 HEIGHT = 400
 WIDTH = 600
 
+data = {}
+data['xs_ys'] = None
+data['mult_num'] = 1
+
 @app.route('/')
 def hello_world():
     layer1 = Linear(n_in=3, n_out=4, activation_function=sigmoid)
     layer2 = Linear(n_in=4, n_out=1, activation_function=sigmoid)
     nn = NeuralNetwork(layers=[layer1, layer2], random_seed=1)
+    data['xs_ys'] = None
+    data['mult_num'] = 1
     return render_template('index.html',
                            height=HEIGHT,
                            width=WIDTH)
@@ -24,10 +30,21 @@ def visualize_neurons():
     num_input = int(input_neurons)
     num_hidden = int(hidden_neurons)
     xs_ys = plot_net(num_input, num_hidden, HEIGHT, WIDTH)
+    data['xs_ys'] = xs_ys
     return render_template('index.html',
                            height=HEIGHT,
                            width=WIDTH,
                            data=xs_ys)
+
+@app.route('/multiply_number/', methods=['GET', 'POST'])
+def multiply():
+    data['mult_num'] *= 2
+    return render_template('index.html',
+                           height=HEIGHT,
+                           width=WIDTH,
+                           data=data['xs_ys'],
+                           mult_num=data['mult_num'])
+
 
 if __name__ == '__main__':
     app.run(debug=True)
