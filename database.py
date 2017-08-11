0623e3c311f45e2e36ec9cf940ed302b6de5f39b
Seth Weidman <sethhweidman@gmail.com>
2017-08-11 18:15:11 -0500

    Neurons retain state in Mongo database

---
+++ database.py
@@ -0,0 +1,72 @@
+# Database code for the DB Forum, full solution!
+
+from pymongo import MongoClient
+
+def find_all_docs():
+    """Return all posts from the 'database', most recent first."""
+    client = MongoClient()
+    db = client.viz_db
+    collection_name = "viz_coll"
+    for document in db[collection_name].find():
+        print(document)
+    client.close()
+    return None
+
+def find_key(key):
+    """Return all posts from the 'database', most recent first."""
+    client = MongoClient()
+    db = client.viz_db
+    collection_name = "viz_coll"
+    output = None
+    for document in db[collection_name].find({"viz_key": key}):
+        output = document['value']
+    client.close()
+    return output
+
+def add_key(key, value):
+    """Return all posts from the 'database', most recent first."""
+    client = MongoClient()
+    db = client.viz_db
+    collection_name = "viz_coll"
+
+    new_json = {"viz_key": key, "value": value}
+    db.command("insert", collection_name,
+               documents=[new_json])
+
+    client.close()
+    return None
+
+def update_key(key, value):
+    """Return all posts from the 'database', most recent first."""
+    client = MongoClient()
+    db = client.viz_db
+    collection_name = "viz_coll"
+
+    new_json = {"viz_key": key, "value": value}
+    db.command("update", collection_name,
+             updates=[{'q': {'viz_key': key},
+                       'u': new_json}])
+
+    client.close()
+    return True
+
+def delete_key(key):
+    """Return all posts from the 'database', most recent first."""
+    client = MongoClient()
+    db = client.viz_db
+    collection_name = "viz_coll"
+
+    db.command("delete", collection_name,
+               deletes=[{'q': {'viz_key': key}}],
+               limit=1)
+
+    client.close()
+    return None
+
+def delete_all_docs():
+    """Return all posts from the 'database', most recent first."""
+    client = MongoClient()
+    db = client.viz_db
+    db.command("dropDatabase")
+    client.close()
+    return None
--- main.py
+++ main.py
@@ -1,50 +1,54 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from neural_net import NeuralNetwork, Linear, sigmoid
 from plot_net import plot_net
+import database as db
 
 app = Flask(__name__)
 
-HEIGHT = 400
-WIDTH = 600
-
-data = {}
-data['xs_ys'] = None
-data['mult_num'] = 1
 
 @app.route('/')
 def hello_world():
-    layer1 = Linear(n_in=3, n_out=4, activation_function=sigmoid)
-    layer2 = Linear(n_in=4, n_out=1, activation_function=sigmoid)
-    nn = NeuralNetwork(layers=[layer1, layer2], random_seed=1)
-    data['xs_ys'] = None
-    data['mult_num'] = 1
+
+    if not db.find_key("dimensions"):
+        db.add_key("dimensions", {'height': 480, 'width': 600})
+    if not db.find_key("data"):
+        db.add_key("data", [])
     return render_template('index.html',
-                           height=HEIGHT,
-                           width=WIDTH)
+                           dimensions=db.find_key('dimensions'),
+                           data=db.find_key('data'))
+
 
 @app.route('/visualize_neurons/', methods=['GET', 'POST'])
 def visualize_neurons():
-    print("The button has been clicked")
+    print("Data upon starting visual:")
+    print(db.find_all_docs())
+    print()
     input_neurons = request.form['input_neurons']
     hidden_neurons = request.form['hidden_neurons']
     num_input = int(input_neurons)
     num_hidden = int(hidden_neurons)
-    xs_ys = plot_net(num_input, num_hidden, HEIGHT, WIDTH)
-    data['xs_ys'] = xs_ys
+    height = db.find_key('dimensions')['height']
+    width = db.find_key('dimensions')['width']
+    xs_ys = plot_net(num_input, num_hidden, height, width)
+    db.update_key('data', xs_ys)
+    print("Data after update:")
+    print(db.find_key('data'))
     return render_template('index.html',
-                           height=HEIGHT,
-                           width=WIDTH,
-                           data=xs_ys)
+                           dimensions=db.find_key('dimensions'),
+                           data=db.find_key('data'))
 
-@app.route('/multiply_number/', methods=['GET', 'POST'])
+
+@app.route('/reset_data/', methods=['GET', 'POST'])
 def multiply():
-    data['mult_num'] *= 2
+    print("The reset data button was clicked")
+    db.delete_all_docs()
+    if not db.find_key("dimensions"):
+        db.add_key("dimensions", {'height': 480, 'width': 600})
+    if not db.find_key("data"):
+        db.add_key("data", [])
     return render_template('index.html',
-                           height=HEIGHT,
-                           width=WIDTH,
-                           data=data['xs_ys'],
-                           mult_num=data['mult_num'])
-
+                           dimensions=db.find_key('dimensions'),
+                           data=db.find_key('data'))
 
 if __name__ == '__main__':
     app.run(debug=True)
